c554147ca5efada4001f5cc0be8944c2fba09d0e
QR <13252155+qrbounty@users.noreply.github.com>
2020-01-20 05:07:53 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -98,12 +98,21 @@ pip3_packages() {
 }
 
 ### Dotfile Stuff ###
+config(){ /usr/bin/git --git-dir=$user_home/.cfg/ --work-tree=$user_home $@; }
 dotfile_copy(){
   [ ! -d "$user_home/.cfg" ] && /bin/su -c "mkdir $user_home/.cfg" - $SUDO_USER
   /bin/su -c "git clone --bare $dotfile_repo $user_home/.cfg" - $SUDO_USER
-  [ ! -d "$user_home/.config-backup" ] && /bin/su -c "mkdir -p $user_home/.config-backup" - $SUDO_USER
+  [ ! -d "$user_home/.config-backup" ] && /bin/su -c "mkdir -p .config-backup" - $SUDO_USER
+  /bin/su -c "/usr/bin/git --git-dir=$user_home/.cfg/ --work-tree=$user_home checkout -f
+  if [ $? = 0 ]; then
+    echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    /bin/su -c "/usr/bin/git --git-dir=$user_home/.cfg/ --work-tree=$user_home checkout 2>&1" - $SUDO_USER | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv $user_home/{} $user_home/.config-backup/{}
+  fi;
+  /bin/su -c "/usr/bin/git --git-dir=$user_home/.cfg/ --work-tree=$user_home checkout" - $SUDO_USER
+  /bin/su -c "/usr/bin/git --git-dir=$user_home/.cfg/ --work-tree=$user_home config status.showUntrackedFiles no" - $SUDO_USER
 }
-
 
 ###  Main  ###
 rule "-"
